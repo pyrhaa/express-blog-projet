ceba9213624f5a32e48d8d91db3960388bbcd834
pyrhaa <sboubtane@protonmail.com>
2021-04-28 16:15:16 +0200

    update pck.json

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then((result) => app.listen(3000));
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log('Server running at http://localhost:3000');
+    })
+  );
 // .catch((err) => console.log(err));
 //register view engine
 app.set('view engine', 'ejs');
